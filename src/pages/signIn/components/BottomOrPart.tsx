3632d2197fc48a5f0060f49ecc8488a0aad723a8
daniella <daniellabotros@gmail.com>
2021-06-15 03:55:04 +0200

    It's working but google svg image won't appear on the buton and not sure how to streach the lines.

---
+++ src/pages/signIn/components/BottomOrPart.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { CheckIcon, MinusIcon } from '@chakra-ui/icons';
+import { HStack} from "@chakra-ui/react";
+import { Box, InputRightElement } from "@chakra-ui/react";
+
+const BottomOrPart = () => {
+    return (
+        <Box textAlign='center'>
+             <HStack direction={["column", "row"]} spacing="202px">
+           <MinusIcon color='#000000' width='240 px'/>
+            <text fontSize='18px'>or</text>
+           <MinusIcon color='#000000' width='240 px'/>
+            </HStack>
+        </Box>
+       
+    )
+}
+
+export default BottomOrPart
--- src/pages/signIn/components/SignInForm.tsx
+++ src/pages/signIn/components/SignInForm.tsx
@@ -1,11 +1,13 @@
 import { Button } from '@chakra-ui/button';
-import { Box } from "@chakra-ui/react"
+import { Box, Icon, InputRightElement, InputLeftElement } from "@chakra-ui/react"
 import { Checkbox} from "@chakra-ui/react"
 import { Input } from "@chakra-ui/react"
 import { Stack} from "@chakra-ui/react"
 import { CheckIcon } from '@chakra-ui/icons';
 import { IconButton } from "@chakra-ui/react";
 import GmailIcon from 'src/assets/GmailIcon.svg';
+import BottomOrPart from './BottomOrPart';
+import { Image } from "@chakra-ui/react";
 import {
   FormControl,
   FormLabel
@@ -24,6 +26,9 @@ const SignInForm = () => {
           borderColor='#31B3C2'
           >
             <Input type='email' placeholder='Email'/>
+            <InputRightElement width="3rem" height="3rem">
+              <CheckIcon color='#31B3C2'/>
+              </InputRightElement>
             
           </FormControl>
   
@@ -34,8 +39,14 @@ const SignInForm = () => {
            borderColor='#31B3C2'
            >
             <Input type='password' placeholder='Password'/>
+            <InputRightElement width="3rem" height="3rem">
+              <CheckIcon color='#31B3C2'/>
+              </InputRightElement>
           </FormControl>
           <Button bgColor='#31B3C2' textColor='#FFFFFF' width='full' mt={4}>Sign In</Button>
+          <BottomOrPart></BottomOrPart>
+          <Button leftIcon={<Image px={[16, 16, 'unset']} mb={[16, 16, 'unset']} src={GmailIcon} objectFit="cover"/>} bgColor='#31B3C2' textColor='#FFFFFF' width='full' mt={4}>Sign Up with Google</Button>
+          
         </form>
       </Box>
     )
