78bb31e9a00c3249e7d7d06d850109c79b171bc6
Hager Eldakroury <hagerdakroury@gmail.com>
2021-06-17 17:14:28 +0200

    Enforce formatting and linting for SignIn validation

--- src/pages/signIn/components/SignInForm.tsx
+++ src/pages/signIn/components/SignInForm.tsx
@@ -1,25 +1,59 @@
 import { Button } from '@chakra-ui/button';
-import { Box, InputRightElement } from '@chakra-ui/react';
+import { Box, Icon, InputRightElement } from '@chakra-ui/react';
 import { Input } from '@chakra-ui/react';
 import { CheckIcon } from '@chakra-ui/icons';
 import GmailIcon from 'src/assets/GmailIcon.svg';
 import BottomOrPart from './BottomOrPart';
-import BottomTextPart from './BottomTextPart';
 import { Image } from '@chakra-ui/react';
 import { FormControl } from '@chakra-ui/react';
+import { useState } from 'react';
+import BottomTextPart from './BottomTextPart';
+///////////////////////////////////////////////////////////////////
+
 const SignInForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  let re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const isInvalid = password === '' || email === '' || !re.test(email);
+  const isInvalidEmail = email === '' || !re.test(email);
+  const isInvalidPassword = password === '';
+
+  const handleSignIn = (event: any) => {
+    event.preventDefault();
+    alert(`Email: ${email} & Password: ${password}`);
+  };
+  let pass;
+  let em;
+  if (!isInvalidEmail) {
+    em = <Icon as={CheckIcon} color="#31B3C2" />;
+  } else {
+    em = <Image id="myImage" src="//:0" />;
+  }
+  if (!isInvalidPassword) {
+    pass = <Icon as={CheckIcon} color="#31B3C2" />;
+  } else {
+    pass = <Image id="myImage" src="//:0" />;
+  }
+
   return (
-    <Box my={8} textAlign="center">
-      <form>
+    <Box my={8} textAlign="center" bgColor="#FFFFFF">
+      <form method="POST" onSubmit={handleSignIn}>
         <FormControl
           width="full"
           borderRadius="4px"
           bgColor="#EFEFEF"
           borderColor="#31B3C2"
         >
-          <Input type="email" placeholder="Email" />
+          <Input
+            type="email"
+            placeholder="Email"
+            value={email}
+            rule={{ required: true }}
+            onChange={(event) => setEmail(event.currentTarget.value)}
+          />
           <InputRightElement width="3rem" height="3rem">
-            <CheckIcon color="#31B3C2" />
+            {em}
           </InputRightElement>
         </FormControl>
 
@@ -30,12 +64,20 @@ const SignInForm = () => {
           bgColor="#EFEFEF"
           borderColor="#31B3C2"
         >
-          <Input type="password" placeholder="Password" />
+          <Input
+            type="password"
+            placeholder="Password"
+            value={password}
+            rule={{ required: true }}
+            onChange={(event) => setPassword(event.currentTarget.value)}
+          />
           <InputRightElement width="3rem" height="3rem">
-            <CheckIcon color="#31B3C2" />
+            {pass}
           </InputRightElement>
         </FormControl>
         <Button
+          type="submit"
+          isDisabled={isInvalid}
           bgColor="#31B3C2"
           textColor="#FFFFFF"
           width="full"
@@ -59,7 +101,7 @@ const SignInForm = () => {
           width="full"
           mt={4}
         >
-          Sign Up with Google
+          Sign In with Google
         </Button>
         <BottomTextPart />
       </form>
