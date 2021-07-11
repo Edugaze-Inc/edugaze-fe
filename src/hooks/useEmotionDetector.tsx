a6ff0e926a114d56b2b732fb688c7802735bf397
Hager Eldakroury <hagerdakroury@gmail.com>
2021-07-11 12:05:19 +0200

    feat(model): Add highest emotion tracking

--- src/hooks/useEmotionDetector.tsx
+++ src/hooks/useEmotionDetector.tsx
@@ -1,20 +1,23 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import * as faceapi from '@vladmandic/face-api';
 
 export function useEmotionDetector() {
+  const [emotion, setEmotion] = useState('');
+
   const videoEl = useRef<HTMLVideoElement | null>(null);
   let requestRef = useRef<number>();
+  async function init() {
+    await faceapi.nets.ssdMobilenetv1.loadFromUri('/model');
+    await faceapi.nets.faceExpressionNet.loadFromUri('/model');
+  }
 
   useEffect(() => {
+    init();
+
     if (!videoEl) {
       return;
     }
     let video = videoEl.current;
-    async function init() {
-      await faceapi.nets.ssdMobilenetv1.loadFromUri('/model');
-      await faceapi.nets.faceExpressionNet.loadFromUri('/model');
-    }
-    init();
 
     navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
       // @ts-ignore: Object is possibly 'null'.
@@ -31,7 +34,23 @@ export function useEmotionDetector() {
           .detectSingleFace(videoEl.current)
           .withFaceExpressions();
 
-        console.log(results?.expressions);
+        if (results?.expressions) {
+          var highestVal = Math.max.apply(
+              null,
+              Object.values(results?.expressions)
+            ),
+            highestEmotion = Object.keys(results?.expressions).find(function (
+              a
+            ) {
+              return results?.expressions[a] === highestVal;
+            });
+        }
+        if (highestEmotion) {
+          let emotion: string = highestEmotion;
+          setEmotion(emotion);
+        } else {
+          setEmotion('out');
+        }
 
         requestRef.current = requestAnimationFrame(step);
       }
@@ -39,6 +58,11 @@ export function useEmotionDetector() {
       requestRef.current = requestAnimationFrame(step);
     }
   }, [videoEl]);
+
+  useEffect(() => {
+    //call analysis api here
+    console.log(emotion);
+  }, [emotion]);
 
   //return <video ref={videoEl} />;
 }
