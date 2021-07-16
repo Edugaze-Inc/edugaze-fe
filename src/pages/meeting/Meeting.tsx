083c26fa5e16561ae925192c666455a71d981217
Ziyad Khalil <ziyadhkhalil@gmail.com>
2021-07-16 18:49:12 +0200

    be integration

--- src/pages/meeting/Meeting.tsx
+++ src/pages/meeting/Meeting.tsx
@@ -1,10 +1,18 @@
 import { Center } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import { useMeeting } from './useMeeting';
-
+import { connect } from 'twilio-video';
+import { useEffect } from 'react';
 export const Meeting = () => {
   const { id } = useParams<{ id: string }>();
   const meetingConfig = useMeeting({ id });
+  // console.log(meetingConfig);
+  useEffect(() => {
+    if (meetingConfig.token)
+      connect(meetingConfig.token)
+        .then((res) => console.log(res))
+        .catch(console.error);
+  }, [meetingConfig.token]);
   return (
     <Center>
       {/* <div>NEW MEETING {id}</div> */}
